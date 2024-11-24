8aa4638db262bfc7f602bf3404bf64a0ac247089
arnav130496 <arnav130496@gmail.com>
2024-11-24 08:50:04 +0530

    added eureka client

--- src/main/java/com/learning/cards/CardsApplication.java
+++ src/main/java/com/learning/cards/CardsApplication.java
@@ -2,8 +2,10 @@ package com.learning.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CardsApplication {
 
 	public static void main(String[] args) {
