4f5a84eb799ed357b7e291ec87167fd5a19a633c
arnav130496 <arnav130496@gmail.com>
2024-12-01 13:53:08 +0530

    zipkin integration for spanID & traceID added

--- src/main/java/com/learning/cards/controller/CardsController.java
+++ src/main/java/com/learning/cards/controller/CardsController.java
@@ -2,6 +2,7 @@ package com.learning.cards.controller;
 
 import com.learning.cards.model.CardsDto;
 import com.learning.cards.service.CardsService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/cards")
+@Slf4j
 public class CardsController {
 
     @Autowired
@@ -16,22 +18,26 @@ public class CardsController {
 
     @PostMapping
     public ResponseEntity<String> createNewCard(@RequestParam String mobileNumber){
+        log.info("Create new card Controller layer {}", mobileNumber);
         cardsService.createNewCard(mobileNumber);
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<CardsDto> getCard(@RequestParam String mobileNumber){
+        log.info("Fetch card Controller layer {}", mobileNumber);
         return new ResponseEntity<>(cardsService.getCardInfo(mobileNumber), HttpStatus.OK);
     }
 
     @PutMapping
     public ResponseEntity<CardsDto> updateCard(@RequestBody CardsDto cardsDto){
+        log.info("Update card controller layer");
         return new ResponseEntity<>(cardsService.updateCardDetails(cardsDto), HttpStatus.OK);
     }
 
     @DeleteMapping
     public ResponseEntity<Boolean> deleteCard(@RequestParam String mobileNumber){
+        log.info("Delete Card Controller Layer, mobile number {}", mobileNumber);
         return new ResponseEntity<>( cardsService.deleteCardInfo(mobileNumber), HttpStatus.OK);
     }
 
